7ca4f4b711232b9063619d94b6d74635627eb378
Lexi Crandall Bradshaw <39276266+LCUT@users.noreply.github.com>
2019-11-14 12:45:17 -0700

    Adding styles for game of thrones

---
+++ main.js
@@ -0,0 +1,47 @@
+var card = document.querySelector('.card');
+card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
+
+class GameOFThrones {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+    get area() {
+        return this.calcArea();
+    }
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+const square = new Rectangle(10,10);
+
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const theData = getAPIData('https://anapioficeandfire.com/api/characters/').then(data => {
+    for (const characters of data.results) {
+        getAPIData(characters.url).then(gotData => {
+            populateDOM(gotData)
+        })
+    }
+})
+
+let mainArea = document.querySelector('main')
+
+function populateDOM(single_character) {
+    let gotCard = document.createElement('div')
+    let gotScene = document.createElement('div')
+    let gotFront = document.createElement('div')
+    
+}
+
